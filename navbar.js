56bfb311e200bbb81da33ff789c0cfbd568f5c0b
florianreibel <68703351+florianreibel@users.noreply.github.com>
2020-08-04 00:18:28 +0200

    newTry to fix

--- navbar.js
+++ navbar.js
@@ -4,11 +4,13 @@ window.onscroll = function() {
     var currentScrollPos = window.pageYOffset;
     if (prevScrollPos > currentScrollPos) {
       document.getElementById("navbar").style.top = "0";
-    } else if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-      if (window.matchMedia("(max-width: 337px)").matches) {
-        document.getElementById("navbar").style.top = "-140px";
-      } else {
-        document.getElementById("navbar").style.top = "-93px";
+    } else {
+      if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        if (window.matchMedia("(max-width: 337px)").matches) {
+          document.getElementById("navbar").style.top = "-140px";
+        } else {
+          document.getElementById("navbar").style.top = "-93px";
+        }
       }
     }
     prevScrollPos = currentScrollPos;
