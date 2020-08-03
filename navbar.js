792387d5dec9712df3c826991041e0445163a7af
florianreibel <68703351+florianreibel@users.noreply.github.com>
2020-08-03 23:49:53 +0200

    hidible navbar

---
+++ navbar.js
@@ -0,0 +1,16 @@
+var prevScrollPos = window.pageYOffset;
+window.onscroll = function() {
+  if (window.matchMedia("(max-width: 768px)").matches) {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollPos > currentScrollPos) {
+      document.getElementById("navbar").style.top = "0";
+    } else {
+      if (window.matchMedia("(max-width: 337px)").matches) {
+        document.getElementById("navbar").style.top = "-140px";
+      } else {
+        document.getElementById("navbar").style.top = "-93px";
+      }
+    }
+    prevScrollPos = currentScrollPos;
+  }
+}
