d177b7a1d19436b4282ba26bd5b9476b795117ac
florianreibel <68703351+florianreibel@users.noreply.github.com>
2020-08-04 00:37:22 +0200

    lastCommitForTonight

--- navbar.js
+++ navbar.js
@@ -1,11 +1,13 @@
 var prevScrollPos = window.pageYOffset;
+// hide the navbar in mobile view, when scrolling down
 window.onscroll = function() {
   if (window.matchMedia("(max-width: 768px)").matches) {
     var currentScrollPos = window.pageYOffset;
     if (prevScrollPos > currentScrollPos) {
       document.getElementById("navbar").style.top = "0";
     } else {
-      if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+      // breakpoint for ios safari
+      if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         if (window.matchMedia("(max-width: 337px)").matches) {
           document.getElementById("navbar").style.top = "-140px";
         } else {
